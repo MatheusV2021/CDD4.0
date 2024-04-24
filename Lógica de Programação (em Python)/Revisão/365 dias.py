9625ca0c09881114c94e698e362da9909c3ee74e
Matheus Vinnycius <80143313+MatheusV2021@users.noreply.github.com>
2024-04-24 16:59:37 -0300

    Add files via upload

--- Lógica de Programação (em Python)/Revisão/365 dias.py
+++ Lógica de Programação (em Python)/Revisão/365 dias.py
@@ -9,15 +9,20 @@ ano_atual = int(input("Digite o ano atual: "))
 idade = ano_atual - ano_nascimento
 
 if (mes_nascimento, dia_nascimento) > (mes_atual, dia_atual):
-  idade = idade - 1
+    idade = idade - 1
 
 mes2 = 12 - mes_nascimento + mes_atual
 dia2 = 30 - dia_nascimento + dia_atual
-dias_totais = idade*365 + mes2*30 + dia2
-dias_meses = dias_totais//30
-dias_horas = dias_totais*24
+dias_totais = idade * 365 + mes2 * 30 + dia2
+dias_meses = dias_totais // 30
+dias_horas = dias_totais * 24
 
-if mes2<0:
-  mes2 = mes2* -1
+if mes2 < 0:
+    mes2 = mes2 * -1
 
-print(f"\nVocê tem {idade} anos, {mes2} meses e {dia2} dias. \nDias de vida: {dias_totais} \nMeses de vida: {dias_meses}\nHoras de vida: {dias_horas}")
+if dia2 > 30:
+    mes2 += 1
+    dia2 -= 30
+
+print(
+    f"\nVocê tem {idade} anos, {mes2} meses e {dia2} dias. \nDias de vida: {dias_totais} \nMeses de vida: {dias_meses}\nHoras de vida: {dias_horas}")
