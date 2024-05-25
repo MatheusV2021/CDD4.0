985a4d80946b048c833cc30529a925c71daec2fb
Matheus Vinnycius <80143313+MatheusV2021@users.noreply.github.com>
2024-05-24 22:49:28 -0300

    Atualizar o Jokenpo.py

--- Desafios/Jokenpo.py
+++ Desafios/Jokenpo.py
@@ -1,23 +1,44 @@
 import random
 
 opcoes = ["Pedra", "Papel", "Tesoura"]
-escolha = random.choice(opcoes)
-jogador = str(input("Escolha entre Pedra, Papel ou Tesoura: "))
+pj1 = 0
+mpj2 = 0
 
-if jogador == escolha:
-    print("Empate")
+while pj1 < 2 and mpj2< 2:
+    escolha = random.choice(opcoes)
+    jogador = str(input("Escolha entre Pedra, Papel ou Tesoura: "))
 
-elif jogador == "Pedra" and escolha == "Tesoura":
-    print(f"Jogador: {jogador} \n Maquina: {escolha} \n Jogador Venceu!")
-elif escolha == "Pedra" and jogador == "Tesoura":
-    print(f"Jogador: {escolha} \n Maquina: {jogador} \n Maquina Venceu!")
+    if jogador not in opcoes:
+        print("Escolha inválida. Tente novamente.")
+        continue  
 
-elif jogador == "Tesoura" and escolha == "Papel":
-    print(f"Jogador: {jogador} \n Maquina: {escolha} \n Jogador Venceu!")
-elif escolha == "Tesoura" and jogador == "Papel":
-    print(f"Jogador: {escolha} \n Maquina: {jogador} \n Maquina Venceu!")
+    if jogador == escolha:
+        print("Empate")
 
-elif jogador == "Papel" and escolha == "Pedra":
-    print(f"Jogador: {jogador} \n Maquina: {escolha} \n Jogador Venceu!")
-elif escolha == "Papel" and jogador == "Pedra":
-    print(f"Jogador: {escolha} \n Maquina: {jogador} \n Maquina Venceu!")
+    elif jogador == "Pedra" and escolha == "Tesoura":
+        print(f"Jogador: {jogador} \n Maquina: {escolha} \n Jogador Venceu!")
+        pj1 += 1
+    elif escolha == "Pedra" and jogador == "Tesoura":
+        print(f"Jogador: {jogador} \n Maquina: {escolha} \n Maquina Venceu!")
+        mpj2 += 1
+
+    elif jogador == "Tesoura" and escolha == "Papel":
+        print(f"Jogador: {jogador} \n Maquina: {escolha} \n Jogador Venceu!")
+        pj1 += 1
+    elif escolha == "Tesoura" and jogador == "Papel":
+        print(f"Jogador: {jogador} \n Maquina: {escolha} \n Maquina Venceu!")
+        mpj2 += 1
+
+    elif jogador == "Papel" and escolha == "Pedra":
+        print(f"Jogador: {jogador} \n Maquina: {escolha} \n Jogador Venceu!")
+        pj1 += 1
+    elif escolha == "Papel" and jogador == "Pedra":
+        print(f"Jogador: {jogador} \n Maquina: {escolha} \n Maquina Venceu!")
+        mpj2 += 1
+
+    print(f"Placar - Jogador: {pj1} x Maquina: {mpj2}")
+
+if pj1 == 2:
+    print("Jogador venceu a melhor de 3!")
+else:
+    print("Maquina venceu a melhor de 3!")
